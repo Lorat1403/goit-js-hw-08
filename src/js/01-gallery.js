593206ddb0001acd69ad1efb60388e5886e5f998
Lora <lorat1403@gmail.com>
2022-07-21 15:34:46 +0300

    mistake

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+import SimpleLightbox from "simple-lightbox";
+import "simple-lightbox/dist/simpleLightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.addEventListener("click", onGalleryClick);
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return ` <div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div> `;
+  })
+  .join("");
+console.log(galleryMarkup);
+
+galleryEl.innerHTML = galleryMarkup;
+
+function onModalOpenClick(el) {
+  const imgTarget = el.target.classList.contains("gallery__image");
+  if (!imgTarget) {
+    return;
+  }
+
+  el.preventDefault();
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionData: "alt",
+});
+console.log(lightbox);
